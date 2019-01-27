a5f3f738c6e45dd5e5bd3b7bac41930a1919bb38
Fabio Bombiela <fabom2013@gmail.com>
2019-01-26 21:06:15 -0500

    Dijkstra with persistence and compression with codification

--- ChallengeDataStructures/src/Graph/Main.java
+++ ChallengeDataStructures/src/Graph/Main.java
@@ -1,34 +1,53 @@
 package Graph;
 
-import com.sun.corba.se.impl.orbutil.graph.Graph;
-
 import java.io.*;
-import java.io.DataOutput;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
 public class Main {
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        File af = new File("t.tmp");
-        if(!af.exists()) {
+        File a = new File("normal.tmp");
+        File b = new File("zip.tmp");
+
+        if(!a.exists()) {  //Question if the program has normal.tmp
         GraphWeight grafo=new GraphWeight();
         grafo.loadgraph("cities.txt");
-        System.out.println(grafo.shortedDistance("colombia"));
-         System.out.println(grafo.shortedPath("colombia", "rusia"));
-        FileOutputStream ostream = new FileOutputStream("t.tmp");
-        ObjectOutputStream p = new ObjectOutputStream(ostream);
+        //Writing normal
+        FileOutputStream in1 = new FileOutputStream("normal.tmp");
+        ObjectOutputStream p = new ObjectOutputStream(in1);
         p.writeObject(grafo);
-
+        //Writing zipped code from normal code
+        FileInputStream in2 = new FileInputStream("normal.tmp");
+        FileOutputStream out2 = new FileOutputStream("zip.tmp");
+        DeflaterOutputStream zip2=new DeflaterOutputStream(out2);
+        int data;
+            while ((data=in2.read())!=-1)
+            {
+                zip2.write(data);
+            }
+            zip2.close();
         }
         else {
-            FileInputStream istream = new FileInputStream("t.tmp");
-            ObjectInputStream p = new ObjectInputStream(istream);
-            GraphWeight vuelta;
-            vuelta = (GraphWeight) p.readObject();
-            System.out.println(vuelta.shortedDistance("rusia"));
+            //Read normal code from normal
+            FileInputStream input = new FileInputStream("normal.tmp");
+            ObjectInputStream p = new ObjectInputStream(input);
+            GraphWeight read = (GraphWeight) p.readObject();
+            //Read zipped code from zip.tmp
+            FileInputStream input2 = new FileInputStream("zip.tmp");
+            InflaterInputStream iis=new InflaterInputStream(input2);
+            ObjectInputStream p2 = new ObjectInputStream(iis);
+            GraphWeight read2 = (GraphWeight) p2.readObject();
+
+            System.out.println(read2.shortedDistance("japon"));
+
+
+
+
+
 
         }
     }
